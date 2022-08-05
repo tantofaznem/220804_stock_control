063acef9c960fc7a3d1665e77cadce82f534f57e
Abd Domingos <abdizzoh@gmail.com>
2022-08-05 16:55:59 +0200

    Create new blade (Dashboard)
    Where all the functions and reports will be.

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+@include('Admin.headerfile')
+    @include('Admin.sidebar')
+
+   <body class="hold-transition login-page">
+          <div class="login-box">
+                  <div class="content-header">
+                         <div class="container-fluid">
+                                  <div class="row mb-2">
+                                           <div class="col-sm-6">
+                                                <h1 class="m-0 text-dark">HERE IS WHERE ALL THE MAGIC WILL HAPPEN...</h1>
+                                           </div>
+                                   </div>
+                         </div>
+                  </div>
+          </div>
+
+          @include('Admin.footer')
+
+   </body
